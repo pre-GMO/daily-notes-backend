b2208bae92570118de48267542d309e3040065a3
s-hirano-ist <s-hirano-ist@outlook.com>
2024-06-29 13:40:48 +0900

    feat: initial setups

---
+++ src/main/kotlin/com/pre_gmo/daily_notes/DailyNotesApplication.kt
@@ -0,0 +1,11 @@
+package com.pre_gmo.daily_notes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DailyNotesApplication
+
+fun main(args: Array<String>) {
+	runApplication<DailyNotesApplication>(*args)
+}
